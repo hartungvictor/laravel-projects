60d7538ec4c5342145715ab426faba50a830133d
hartungvictor <victor_hartung@hotmail.com>
2023-05-05 18:01:34 -0300

    first one

--- locadora_carros/routes/api.php
+++ locadora_carros/routes/api.php
@@ -1,7 +1,16 @@
 <?php
 
+
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\CarroController;
+use App\Http\Controllers\LocacaoController;
+use App\Http\Controllers\ModeloController;
+use App\Http\Controllers\MarcaController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +26,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('cliente', ClienteController::class);
+Route::apiResource('carro', CarroController::class);
+Route::apiResource('locacao', LocacaoController::class);
+Route::apiResource('marca', MarcaController::class);
+Route::apiResource('modelo', ModeloController::class);
